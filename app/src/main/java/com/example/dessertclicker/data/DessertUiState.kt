f5c575757f9c5d95e2d0d2a033008c397ce7d477
Mohammad Mustafa Hakimi <147371977+MohamammadMustafaHakimi@users.noreply.github.com>
2025-10-07 04:38:03 +0200

    Ui state data class created. tbd: viewmodel initiation. 2. migration phase 1

---
+++ app/src/main/java/com/example/dessertclicker/data/DessertUiState.kt
@@ -0,0 +1,8 @@
+package com.example.dessertclicker.data
+
+data class DessertUiState(
+    val dessertsSold: Int = 0,
+    val revenue: Int = 0,
+    val dessertImageId: Int = 0,
+    val currentDessertPrice: Int = 0,
+)
